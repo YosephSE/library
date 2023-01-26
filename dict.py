52311e3b79a1e1cdf3a9a1789206255029f2ac86
YosephSE <yoseph.kedir10@gmail.com>
2023-01-26 19:54:50 +0300

    commited

---
+++ dict.py
@@ -0,0 +1,12 @@
+books = {
+    'Eat that frog': 10,
+    'Atomic habits': 29,
+    'Rich dad poor dad': 12,
+    'Why we sleep': 10,
+    'Limitless': 22,
+    'The power of now': 9,
+    'The 7 habits of highly effective people': 21,
+    'Becoming': 32,
+    'The psychology of money': 18,
+    'Make time': 17
+}
---
+++ main.py
@@ -0,0 +1,35 @@
+# l = [4, 6, 2, 1, 90]
+# def sorter(l):
+#     sorted_list = []
+#     while len(l) > 0:
+#         small = l[0]
+#         for num in l:
+#             if num < small:
+#                 small = num
+#                 sorted_list.append(small)
+#                 l.pop(l.index[small])
+#     print(sorted_list)
+#
+# sorter(l)
+
+# def calculate(x, y , opera):
+#     return eval(f'{x}{opera}{y}')
+#
+# print(calculate(4, 5, '+'))
+#
+# def opera_finder(num1, num2, result):
+#     if num1 + num2 == result:
+#         return 'Addition'
+#     elif num1 - num2 == result:
+#         return 'Subtraction'
+#     elif num1 * num2 == result:
+#         return 'Multiplication'
+#     elif num1 / num2 == result:
+#         return 'Division'
+#     else:
+#         return 'Wrong input'
+#
+#
+# print(opera_finder(21, 3, 7))
+
+
